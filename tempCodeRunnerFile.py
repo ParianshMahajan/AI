7ee360fdf3155479ab6eedaa706b80712f9d3380
Pariansh Mahajan <116679976+ParianshMahajan@users.noreply.github.com>
2024-05-08 08:55:43 +0530

    vcbd

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-def heuristic(graph,current_node,visited,start_node):
-    unvisited = set(graph.keys()) - visited
-    if len(unvisited)==0:
-        return graph[current_node][start_node]
-    return min(graph[current_node].get(n,float('inf')) for n in unvisited)
