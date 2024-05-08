e48a0fd55826af05887aca735cbedac7ce92ffa1
Pariansh Mahajan <116679976+ParianshMahajan@users.noreply.github.com>
2024-05-08 06:44:56 +0530

    vrfc

--- travelling_Salesman.py
+++ travelling_Salesman.py
@@ -1,39 +1,35 @@
 import itertools
 
-class TSP:
-    def __init__(self, graph, start):
-        self.graph = graph
-        self.start = start
-        self.visited = set()
-        self.path = []
-        self.total_cost = 0
+def heuristic(graph, visited, current_node, start):
+    unvisited = set(graph.keys()) - visited
+    if len(unvisited) == 0:
+        return graph[current_node][start]
+    return min(graph[current_node][n] for n in unvisited)
 
-    def heuristic(self, node):
-        # Calculate the minimum spanning tree (MST) heuristic
-        unvisited = set(self.graph.keys()) - self.visited
-        if len(unvisited) == 0:
-            return self.graph[node][self.start]
-        return min(self.graph[node][n] for n in unvisited)
+def a_star(graph, start):
+    visited = set()
+    path = []
+    total_cost = 0
+    open_list = [(0, start)]
 
-    def a_star(self):
-        open_list = [(0, self.start)]
-        while open_list:
-            cost, current_node = min(open_list)
-            open_list.remove((cost, current_node))
-            if current_node in self.visited:
-                continue
-            self.path.append(current_node)
-            self.visited.add(current_node)
-            if len(self.visited) == len(self.graph):
-                self.total_cost += self.graph[current_node][self.start]
-                self.path.append(self.start)
-                return self.path, self.total_cost
-            for neighbor, distance in self.graph[current_node].items():
-                if neighbor not in self.visited:
-                    heuristic = self.heuristic(neighbor)
-                    open_list.append((distance + heuristic, neighbor))
-            self.total_cost += cost
-        return None, None
+    while open_list:
+        cost, current_node = min(open_list)
+        open_list.remove((cost, current_node))
+        if current_node in visited:
+            continue
+        path.append(current_node)
+        visited.add(current_node)
+        if len(visited) == len(graph):
+            total_cost += graph[current_node][start]
+            path.append(start)
+            return path, total_cost
+        for neighbor, distance in graph[current_node].items():
+            if neighbor not in visited:
+                h = heuristic(graph, visited, neighbor, start)
+                open_list.append((distance + h, neighbor))
+        total_cost += cost
+
+    return None, None
 
 # Example usage
 graph = {
@@ -43,7 +39,6 @@ graph = {
     'D': {'A': 20, 'B': 25, 'C': 30}
 }
 start_node = 'A'
-tsp = TSP(graph, start_node)
-path, cost = tsp.a_star()
+path, cost = a_star(graph, start_node)
 print("Path:", path)
 print("Total Cost:", cost)
